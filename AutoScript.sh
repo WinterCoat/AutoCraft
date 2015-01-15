5d77b9006ca8bdc5b439c7d5decd9d9a5b3609f1
root <piepieninja@gmail.com>
2015-01-15 02:37:51 +0000

    commands register and perform, parsing and delivery to MC server incomplete

--- AutoScript.sh
+++ AutoScript.sh
@@ -1,8 +1,8 @@
 #The Great AutoScripter. Welcome to the mind of God
-#screen -S 16471.pts-0.RedCraft -X stuff 'say Linux server says hi\n'
+#screen -S 19168.RedCraft -X stuff 'say Linux server says hi\n'
 
 #uncomment if running this initally. For Testing keep this commented... 
-#screen -S 16471.pts-0.RedCraft -L -X stuff 'java -Xmx1024M -Xms1024M -jar minecraft_server.jar nogui'
+#screen -S 19168.RedCraft -L -X stuff 'java -Xmx1024M -Xms1024M -jar minecraft_server.jar nogui'
 
 echo "                                         
  _____     _       _____         ___ _   
@@ -14,6 +14,7 @@ sleep 1
 echo '...'
 
 #For automatic resets
+#Global Variables
 SEC=0
 MIN=0
 HOU=0
@@ -21,7 +22,7 @@ MSG=0
 TEMP=""
 
 echo 'AutoCraft starting'
-screen -S 16471.pts-0.RedCraft -X stuff '/say Linux AutoCraft Script: starting\n'
+screen -S 19168.RedCraft -X stuff '/say Linux AutoCraft Script: starting\n'
 
 while :
 do
@@ -34,29 +35,22 @@ do
     then
 	if [[ $text == *"$"* ]];
 	then
-	    echo 'command registered'
+	    screen -S 19352.MCShell -L -X stuff "$snooper\n"
+	    ret=$(tail -5 /home/minecraft/screenlog.0)
+	    echo $ret
+	    clean=$(ruby /home/minecraft/AutoCraft/cleaner.rb "$ret")
+	    echo $clean
+	    screen -S 19168.RedCraft -X stuff "$clean\n"
 	else
-	    screen -S 16471.pts-0.RedCraft -X stuff "$snooper\n"
+	    screen -S 19168.RedCraft -X stuff "$snooper\n"
 	fi
     fi
 
     #end snoop
-
-
-    #commands here
-
-    #if [[ $text == *" $" ]];
-    #then
-	#echo 'command registered'
-    #fi
-    
-    #end commands
     
     if [ $SEC -eq 60 ];
     then
     #increment in case it has been a minute
-	#echo 'it has been a minute'
-	#screen -S 16471.pts-0.RedCraft -X stuff 'say Running Diagnostics...'
 	SEC=0
 	MIN=$((MIN + 1))
     fi
@@ -65,8 +59,8 @@ do
     then
     #increment in case it have been an hour
 	echo 'it has been and hour'
-	screen -S 16471.pts-0.RedCraft -X stuff 'say Linux Hourly Save...\n'
-	screen -S 16471.pts-0.RedCraft -X stuff 'save-all\n'
+	screen -S 19168.RedCraft -X stuff 'say Linux Hourly Save...\n'
+	screen -S 19168.RedCraft -X stuff 'save-all\n'
 	MIN=0
 	MSG=0
 	HOU=$((HOU + 1))
@@ -75,32 +69,32 @@ do
     if [ $HOU -eq 24 ];
     then
 	echo 'it has been a day'
-	screen -S 16471.pts-0.RedCraft -X stuff 'say Linux will refresh in 10 minutes\n'
+	screen -S 19168.RedCraft -X stuff 'say Linux will refresh in 10 minutes\n'
 	sleep 5m
-	screen -S 16471.pts-0.RedCraft -X stuff 'say Linux will refresh in 5 minutes\n'
+	screen -S 19168.RedCraft -X stuff 'say Linux will refresh in 5 minutes\n'
 	sleep 4m
-	screen -S 16471.pts-0.RedCraft -X stuff 'say Linux will refresh in 1 minutes\n'
+	screen -S 19168.RedCraft -X stuff 'say Linux will refresh in 1 minutes\n'
 	sleep 50s
-	screen -S 16471.pts-0.RedCraft -X stuff 'say Linux will refresh in 10 seconds\n'
+	screen -S 19168.RedCraft -X stuff 'say Linux will refresh in 10 seconds\n'
 	sleep 5s
-	screen -S 16471.pts-0.RedCraft -X stuff 'say Linux will refresh in 5 seconds\n'
+	screen -S 19168.RedCraft -X stuff 'say Linux will refresh in 5 seconds\n'
 	sleep 1s
-	screen -S 16471.pts-0.RedCraft -X stuff 'say 4\n'
+	screen -S 19168.RedCraft -X stuff 'say 4\n'
 	sleep 1s
-	screen -S 16471.pts-0.RedCraft -X stuff 'say 3\n'
+	screen -S 19168.RedCraft -X stuff 'say 3\n'
 	sleep 1s
-	screen -S 16471.pts-0.RedCraft -X stuff 'say 2\n'
+	screen -S 19168.RedCraft -X stuff 'say 2\n'
 	sleep 1s
-	screen -S 16471.pts-0.RedCraft -X stuff 'say 1\n'
+	screen -S 19168.RedCraft -X stuff 'say 1\n'
 	
 	#save all the things
-	screen -S 16471.pts-0.RedCraft -X stuff 'save-all\n'
+	screen -S 19168.RedCraft -X stuff 'save-all\n'
 	#end it
-	screen -S 16471.pts-0.RedCraft -X stuff 'stop\n'
+	screen -S 19168.RedCraft -X stuff 'stop\n'
 
 	sleep 15s
 	#start it again
-	screen -S 16471.pts-0.RedCraft -L -X stuff 'java -Xmx1024M -Xms1024M -jar minecraft_server.jar nogui\n'
+	screen -S 19168.RedCraft -X stuff 'java -Xmx1024M -Xms1024M -jar minecraft_server.jar nogui\n'
 	#reset variables
 	SEC=0
 	MIN=0
--- snoop.rb
+++ snoop.rb
@@ -11,13 +11,19 @@ user = nil
 if (n and m) != nil
   user = ARGV[0][n+1..m-1]
 end
-if user == nil
+if (user == nil and !input.include? "joined the game")
   user = input[34..input.rindex(':')-1]
+end
+if user == nil
+  user = input[33..input.rindex(" joined")-1]
 end
 
 
-if !input.include? "[Server]" or input.include? "Server Test:"
-  
+if input.include? "$" and (!input.include? "[Server]" or input.include? "Server Test:" or user == "pipininja")
+  #Bash Commands Proccessed here
+  @cmd = input[input.index('$')+1..input.length]
+  puts @cmd
+elsif !(input.include? "[Server]") or input.include? "Server Test:"
   if input.include? "linux what time is it" or input.include? "linux time" or input.include? "what time is it linux"
     time = Time.new
     puts "/say System time is: " + time.inspect
@@ -25,9 +31,9 @@ if !input.include? "[Server]" or input.include? "Server Test:"
     puts "/say hello " + user
   elsif input.include? "what is life" or input.include? "meaning of life" or input.include? "answer to life"
     puts "/say 42"
-  elsif input.include? "fuck you linux" or input.include? "fuck linux" or input.include? "linux sucks"
-    puts "/say fuck you " + user
-    puts "/kick " + user
+  elsif input.include? "fuck you linux" or input.include? "fuck linux" or input.include? "linux sucks" or (input.include? "fuck" and input.include? "linux")
+    puts "/say Feel My Wrath " + user
+    puts "/kill " + user 
   elsif input.include? "Set own game mode to Creative"
     puts "/tell " + user + " be thoughtfull of your power"
   elsif input.include? "save me linux" or input.include? "linux save me"
@@ -43,15 +49,19 @@ if !input.include? "[Server]" or input.include? "Server Test:"
   elsif input.include? "microsoft"
     puts "UNIX is superiour to DOS in all aspects"
   elsif input.include? "linux what are the rules" or (input.include? "linux" and input.include? "rules") or input.include? "joined the game"
-    puts "/say DA RULEZ:\n/say 1. The Spawn Island (Nexus) is owned by the Ops. Feel Free to take supplies, but be nice and leave some for others. Go start your own settlement!\n/say 2. City States Outside of Nexus will determine their own governments\n/say 3. Diamonds are the official trading currency with 25 iron = 1 diamond"
-  elsif input.include? "linux what are the comands" or input.include? "linux show commands" or (input.include? "linux" and input.include? "commands") or input.include? "linux"
+    puts "/tell " + user + " ======> Welcome: " + user + " <======"
+    puts "/tell " + user + 
+      " DA RULEZ:\n/tell " + user + 
+      " 1. The Spawn Island (Nexus) is owned by the Ops. Feel Free to take supplies, but be nice and leave some for others. Go start your own settlement!\n/tell " + user + 
+      " 2. City States Outside of Nexus will determine their own governments\n/tell " + user + 
+      " 3. Diamonds are the official trading currency with 25 iron = 1 diamond"
+  elsif input.include? "linux what are the comands" or input.include? "linux show commands" or (input.include? "linux" and input.include? "commands") or input.include? "linux" or input.include? "linux help"
     puts "/say try the following:\n/say => hello linux\n/say => what time is it linux\n/say => linux what are the rules\n/say => Bash commands starting with identifier $"
+    elsif input.include? "drowned" or input.include? "blown up" or input.include? "blew up" or input.include? "fell" or input.include? "killed" or input.include? "slain" or input.include? "tried to swim" or input.include? "shot" or input.include? "death"
+    puts "/say lol"
   else
-    puts "0"
+  puts "0"
   end
-elsif input.include? "$"
-  #Bash Commands Proccessed here
-
 else
   puts "0"
 end
